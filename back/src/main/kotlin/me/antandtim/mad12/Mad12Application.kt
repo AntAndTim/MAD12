1f0c1f568d5abaec28a832d83e3dee8515f14421
Anton Timchenko <a.timchenko@tinkoff.ru>
2020-12-10 23:48:45 +0300

    feat(back): added initial version

---
+++ back/src/main/kotlin/me/antandtim/mad12/Mad12Application.kt
@@ -0,0 +1,11 @@
+package me.antandtim.mad12
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Mad12Application
+
+fun main(args: Array<String>) {
+    runApplication<Mad12Application>(*args)
+}
---
+++ back/src/test/kotlin/me/antandtim/mad12/Mad12ApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.antandtim.mad12
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Mad12ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
